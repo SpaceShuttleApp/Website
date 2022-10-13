fe840082769ede706d2db7b331db98de7a586f86
Sofa <72651859+SlumberDemon@users.noreply.github.com>
2022-10-14 00:14:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -18,8 +18,6 @@ async def home(request: fastapi.Request):
 
 @app.get("/install", response_class=fastapi.responses.HTMLResponse)
 def install(request: fastapi.Request):
-    return fastapi.responses.HTMLResponse(
-        """
-        <meta http-equiv="refresh" content="0; url = https://alpha.deta.space/discovery/spaceshuttle-sbm-v0.1.0" />
-        """
+    return fastapi.responses.RedirectResponse(
+        "https://alpha.deta.space/discovery/spaceshuttle-sbm-v0.1.0"
     )

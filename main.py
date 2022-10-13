a3281a600d3b997ee1cde4cd494b6421e6950f0d
Sofa <Nextil3o3@gmail.com>
2022-10-13 22:52:58 +0200

    /install page

--- main.py
+++ main.py
@@ -14,3 +14,12 @@ app.mount("/assets", StaticFiles(directory="assets"), name="assets")
 @app.get("/")
 async def home(request: fastapi.Request):
     return pages.TemplateResponse("home.html", {"request": request})
+
+
+@app.get("/install", response_class=fastapi.responses.HTMLResponse)
+def install(request: fastapi.Request):
+    return fastapi.responses.HTMLResponse(
+        """
+        <meta http-equiv="refresh" content="0; url = https://alpha.deta.space/discovery/spaceshuttle-sbm-v0.1.0" />
+        """
+    )

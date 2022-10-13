d8f40fdaed5a71b1c8500ab082ae2efa6b286ed9
LemonPi314 <49930425+LemonPi314@users.noreply.github.com>
2022-10-13 18:33:59 -0400

    Clean up main.py

--- main.py
+++ main.py
@@ -1,23 +1,19 @@
-import fastapi
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from fastapi.middleware.cors import CORSMiddleware
 
-
-app = fastapi.FastAPI()
+app = FastAPI()
 pages = Jinja2Templates(directory="static")
-app.add_middleware(CORSMiddleware, allow_origins=["*"])
 app.mount("/styles", StaticFiles(directory="styles"), name="styles")
 app.mount("/assets", StaticFiles(directory="assets"), name="assets")
 
 
-@app.get("/")
-async def home(request: fastapi.Request):
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
     return pages.TemplateResponse("home.html", {"request": request})
 
 
-@app.get("/install", response_class=fastapi.responses.HTMLResponse)
-def install(request: fastapi.Request):
-    return fastapi.responses.RedirectResponse(
-        "https://alpha.deta.space/discovery/spaceshuttle-sbm-v0.1.1"
-    )
+@app.get("/install", response_class=RedirectResponse)
+async def install():
+    return RedirectResponse("https://alpha.deta.space/discovery/spaceshuttle-sbm-v0.1.0")

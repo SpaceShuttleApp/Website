2c36aca7b0d0ace37b660092a62effe405a973cb
LemonPi314 <49930425+LemonPi314@users.noreply.github.com>
2022-10-13 22:30:50 -0400

    Move files to static and templates directories

--- main.py
+++ main.py
@@ -4,14 +4,13 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
-pages = Jinja2Templates(directory="static")
-app.mount("/styles", StaticFiles(directory="styles"), name="styles")
-app.mount("/assets", StaticFiles(directory="assets"), name="assets")
+pages = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
 @app.get("/", response_class=HTMLResponse)
-async def home(request: Request):
-    return pages.TemplateResponse("home.html", {"request": request})
+async def index(request: Request):
+    return pages.TemplateResponse("index.html", {"request": request})
 
 
 @app.get("/install", response_class=RedirectResponse)

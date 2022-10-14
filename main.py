bb434bb94ff35cd63454fc09910595bcc8ef7d47
Sofa <Nextil3o3@gmail.com>
2022-10-14 08:50:18 +0200

    Changed imports

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse, RedirectResponse
+import fastapi
+from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
-app = FastAPI()
+app = fastapi.FastAPI()
 pages = Jinja2Templates(directory="templates")
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
 @app.get("/", response_class=HTMLResponse)
-async def index(request: Request):
+async def index(request: fastapi.Request):
     return pages.TemplateResponse("index.html", {"request": request})
 
 
